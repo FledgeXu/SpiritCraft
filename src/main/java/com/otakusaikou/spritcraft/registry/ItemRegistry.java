7e0d4afe5c1b26e063e8b442cb296684fdbdfdd8
FledgeXu <xzk0701@gmail.com>
2020-05-31 15:51:48 +0800

    add the Spiriter.

--- src/main/java/com/otakusaikou/spritcraft/registry/ItemRegistry.java
+++ src/main/java/com/otakusaikou/spritcraft/registry/ItemRegistry.java
@@ -7,8 +7,6 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.function.Supplier;
-
 public class ItemRegistry {
     private static final Item.Properties DEFAULT_PROPERTIES = new Item.Properties().group(ModGroup.DEFAULT_GROUP);
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, ModConstants.MOD_ID);
@@ -17,5 +15,5 @@ public class ItemRegistry {
     public static final RegistryObject<Item> waterCrystal = ITEMS.register("water_crystal", () -> new Item(DEFAULT_PROPERTIES));
     public static final RegistryObject<Item> fireCrystal = ITEMS.register("fire_crystal", () -> new Item(DEFAULT_PROPERTIES));
     public static final RegistryObject<Item> earthCrystal = ITEMS.register("earth_crystal", () -> new Item(DEFAULT_PROPERTIES));
-
+    public static final RegistryObject<Item> spiriter = ITEMS.register("spiriter", () -> new Item(DEFAULT_PROPERTIES));
 }
